17399e1d2cf951b72f42beca65aa813eb7b73686
fcd <1352288+femnad@users.noreply.github.com>
2026-05-17 23:03:43 +0100

    add completion command

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,26 @@
-use clap::{Args, Parser, Subcommand};
+use clap::{Args, Command, CommandFactory, Parser, Subcommand};
+use clap_complete::{Generator, Shell, generate};
+use std::io;
 
 mod latest_kernel;
 mod prefix;
 
 #[derive(Debug, Parser)]
-#[command(name = "uno", version, about = "Do everything*", long_about = "*Do some of the things")]
+#[command(
+    name = "uno",
+    version,
+    about = "Do everything*",
+    long_about = "*Do some of the things"
+)]
 struct Cli {
     #[command(subcommand)]
     command: Commands,
+}
+
+#[derive(Args, Debug)]
+struct GenerateArgs {
+    #[arg(help = "Shell name")]
+    shell: Shell,
 }
 
 #[derive(Args, Debug)]
@@ -18,17 +31,26 @@ struct PrefixArgs {
 
 #[derive(Debug, Subcommand)]
 enum Commands {
+    #[command(about = "Generate completions")]
+    Generate(GenerateArgs),
     #[command(about = "Check if latest kernel is running", alias = "lk")]
     LatestKernel,
     #[command(about = "Show prefix within repo", alias = "prf")]
     Prefix(PrefixArgs),
 }
 
+fn print_completions<G: Generator>(generator: G, cmd: &mut Command) {
+    generate(generator, cmd, cmd.get_name().to_string(), &mut io::stdout());
+}
+
 fn main() {
     let cli = Cli::parse();
 
     match &cli.command {
-        Commands::LatestKernel => { latest_kernel::check()}
+        Commands::Generate(generate_args) => {
+            print_completions(generate_args.shell, &mut Cli::command())
+        },
+        Commands::LatestKernel => latest_kernel::check(),
         Commands::Prefix(prefix_args) => prefix::get(prefix_args.relative_path.clone()),
     }
 }
