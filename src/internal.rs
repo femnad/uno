e6193023777843824a4e9570e6188e0647d20f60
fcd <1352288+femnad@users.noreply.github.com>
2026-08-23 01:44:00 +0100

    determine clipboard copy command from the environment

--- src/internal.rs
+++ src/internal.rs
@@ -2,9 +2,20 @@ use std::env;
 use std::io::Write;
 use std::process::{Command, Stdio};
 
+fn get_clipboard_command() -> Result<String, String> {
+    let session_type = env::var("XDG_SESSION_TYPE").expect("Unable to determine session type");
+    let session_type = session_type.as_str();
+    match session_type {
+        "wayland" => Ok("wl-copy".to_string()),
+        "x11" => Ok("xclip".to_string()),
+        _ => Err("Unrecognized session type".to_string()),
+    }
+}
+
 fn copy_to_clipboard(content: &String) {
-    let cmd = Command::new("xclip").stdin(Stdio::piped()).spawn();
-    if let Some(mut stdin) = cmd.unwrap().stdin.take() {
+    let cmd = get_clipboard_command().expect("Unable to determine clipboard command");
+    let cmd = Command::new(cmd).stdin(Stdio::piped()).spawn();
+    if let Some(mut stdin) = cmd.expect("Unable to run clipboard command").stdin.take() {
         stdin.write_all(content.as_bytes()).unwrap();
     }
 }
@@ -14,7 +25,11 @@ fn copy_tmux_buffer(content: &String) {
         return;
     }
 
-    Command::new("tmux").arg("set-buffer").arg(format!("{}", content)).spawn().unwrap();
+    Command::new("tmux")
+        .arg("set-buffer")
+        .arg(format!("{}", content))
+        .spawn()
+        .unwrap();
 }
 
 pub fn copy(content: &String) {
